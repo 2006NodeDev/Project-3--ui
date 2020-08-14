3b44ae4afa7f90db34c4e69f5b5328b55b9e61cf
ceciliapichardo <cecmaria23@gmail.com>
2020-08-14 14:22:06 -0700

    implemented Auth0 login to frontend

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { UpdatePasswordComponent } from './components/UpdatePasswordComponent/UpdatePasswordComponent';
+import { Auth0LoginComponent } from './components/Auth0LoginComponent/Auth0LoginComponent';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Router>
           <h1>Project is Running...</h1>
           <Route path='/updatePassword' component={UpdatePasswordComponent}/>
+          <Auth0LoginComponent />
         </Router>
       </Provider>
     </div>
