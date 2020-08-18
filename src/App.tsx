23d4beb06b0f56d8f4a15823c91b9c78e8716ebb
benjamin skinner <benjamin.skinner15@gmail.com>
2020-08-18 16:25:07 -0400

    updating frontend, display options for assocates

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { store } from './store';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { UpdatePasswordComponent } from './components/UpdatePasswordComponent/UpdatePasswordComponent';
 import { AssociateInfoComponent } from './components/AssociateInfoComponent/AssociateInfoComponent';
-import { AllAssociatesComponent } from './components/AllAssociateComponent/AllAssociateComponent'
 import { NavBarComponent } from './components/NavbarComponent/NavbarComponent';
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
         <NavBarComponent user={null}/>
           <Route path='/updatePassword' component={UpdatePasswordComponent}/>
           <Route path='/associateInfo' component={AssociateInfoComponent}/>
-          <Route path='/allAssociate' component={AllAssociatesComponent}/>
         </Router>
       </Provider>
     </div>
--- src/components/AssociateInfoComponent/AssociateInfoComponent.tsx
+++ src/components/AssociateInfoComponent/AssociateInfoComponent.tsx
@@ -1,19 +1,43 @@
 import React from 'react'
 import { FunctionComponent } from "react";
-import { Button } from '@material-ui/core';
+import { Button, makeStyles, Theme, createStyles, FormControl, InputLabel, Select } from '@material-ui/core';
 
 
-export const AssociateInfoComponent:FunctionComponent = (props) => {
-return(
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }),
+);
+
+export const AssociateInfoComponent:FunctionComponent<any> = (props) => {
+  const classes = useStyles();
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setAge(event.target.value as string);
+  };
+
+  return (
     <div>
-        <h1>
-            {/*Associate Info Goes here!!!*/}
-            {/*<Button variant="contained">Default</Button>*/}
-            <Button variant="contained" color="primary">
-             Associate info
-            </Button>
-        </h1>
+
+
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          onChange={handleChange}
+        >
+        </Select>
+      </FormControl>
     </div>
-)
+  );
 }
 
