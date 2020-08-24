88ead6874f3ef1266be4b43c329c0ef00cb39495
Younus <alia.younus@accenture.com>
2020-08-24 12:23:36 -0500

    merge changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ import { store } from './store';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import { UpdatePasswordComponent } from './components/UpdatePasswordComponent/UpdatePasswordComponent'
 import { AssociateInfoComponent } from './components/AssociateInfoComponent/AssociateInfoComponent';
-<<<<<<< HEAD
-import { AllAssociatesComponent } from './components/AllAssociateComponent/AllAssociateComponent'
-import { BatchbyTrainerEmailComponent } from  './components/BatchbyTrainerComponent/BatchbyTrainerEmailComponent';
-=======
->>>>>>> f6175259555ad079f8aa3afe04565fd95d65bf44
 import { NavBarComponent } from './components/NavbarComponent/NavbarComponent';
 import { CurrentBatchesComponent } from './components/CurrentBatchesComponent/CurrentBatches';
 import HomePageComponent from './components/HomePageComponent/HomePageComponent';
@@ -31,21 +26,6 @@ function App() {
 
           <Route path='/updatePassword' component={UpdatePasswordComponent}/>
           <Route path='/associateInfo' component={AssociateInfoComponent}/>
-<<<<<<< HEAD
-          <Route path='/allAssociate' component={AllAssociatesComponent}/>
-          <Route path='/currentBatches' component={CurrentBatchesComponent}/>
-          <Route path='/batchInfo' component={BatchbyTrainerEmailComponent} />
-          <Route path='/allProfile' component={AllProfileComponent}/>
-
-         
-
-          <Route path='/home'>
-          <WelcomeComponent/>
-        <HomePageComponent/>
-         </Route>
-
-=======
->>>>>>> f6175259555ad079f8aa3afe04565fd95d65bf44
         </Router>
       </Provider>
     </div>
