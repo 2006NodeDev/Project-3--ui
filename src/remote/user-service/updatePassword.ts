49bf830b2fe35d1093732c19d9abd4f7a366c795
Laura Shears <52978933+shearsl@users.noreply.github.com>
2020-08-25 15:17:24 -0400

    stuff from development that is not letting me switch branches

---
+++ src/remote/user-service/updatePassword.ts
@@ -0,0 +1,14 @@
+import { userServiceBaseClient} from '../index'
+
+export const updatePasswordRemote = async (body:any) => {
+try { 
+        //needs to include path to that service (the repo name) as the base path
+        let response = await userServiceBaseClient.patch('/user-service/updatePassword', body);
+
+        console.log(response);
+        return response.data
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
+}
