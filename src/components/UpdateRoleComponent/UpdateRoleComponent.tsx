444b07ced20a0a6f6742c4b7f432cc40e105b2a6
ABlanton3 <a.m.blanton@gmail.com>
2020-08-25 11:30:25 -0500

    update role ui changes

--- src/components/UpdateRoleComponent/UpdateRoleComponent.tsx
+++ src/components/UpdateRoleComponent/UpdateRoleComponent.tsx
@@ -5,32 +5,49 @@ import { createStyles, makeStyles, Theme, InputLabel, Select, FormControl } from
 import { IState } from '../../reducers';
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120,
-    },
-    selectEmpty: {
-      marginTop: theme.spacing(2),
-    },
-  }),
+    createStyles({
+        formControl: {
+            margin: theme.spacing(1),
+            minWidth: 120,
+        },
+        selectEmpty: {
+            marginTop: theme.spacing(2),
+        },
+        btn: {
+            background: "#61dafb",
+            borderColor: "#61dafb"
+        },
+        input: {
+            '&:before': {
+                borderRadius: 4,
+                borderColor: "#000000",
+                boxShadow: "#000000"
+            },
+            '&:after': {
+                borderRadius: 4,
+                borderColor: "#000000",
+                boxShadow: "#000000",
+                name: "#000000"
+            }
+        }
+    }),
 );
 
-export const UpdateRoleComponent:FunctionComponent<any> = (props) => {
+export const UpdateRoleComponent: FunctionComponent<any> = (props) => {
     const classes = useStyles();
     const [role, changeRole] = useState('');
 
-    const updateRole = (e:any) =>{
+    const updateRole = (e: any) => {
         e.preventDefault();
         changeRole(e.currentTarget.value);
         console.log(role);
-        
+
     }
 
-    let currentUser = useSelector((state:IState)=>{
+    let currentUser = useSelector((state: IState) => {
         return state.loginState.currentUser
     })
-    
+
     let dispatch = useDispatch();
 
     const submitRole = async (e: SyntheticEvent) => {
@@ -47,7 +64,7 @@ export const UpdateRoleComponent:FunctionComponent<any> = (props) => {
         <div>
             <FormControl className={classes.formControl}>
                 <InputLabel htmlFor="age-native-simple">Role</InputLabel>
-                <Select native value={role} onChange={updateRole} inputProps={{
+                <Select className={classes.input} inputProps={{
                     name: 'role',
                     id: 'age-native-simple',
                 }}>
@@ -56,7 +73,7 @@ export const UpdateRoleComponent:FunctionComponent<any> = (props) => {
                     <option value='Admin'>Admin</option>
                 </Select>
             </FormControl>
-            <button className="btn " type="submit" onClick={submitRole}> Submit </button>
+            <button className={classes.btn} color="default" type="submit" > Submit </button>
         </div>
     )
 
