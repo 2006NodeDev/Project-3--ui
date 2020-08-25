6c556ece1a396206f2145129e36a846bc8583deb
ABlanton3 <a.m.blanton@gmail.com>
2020-08-25 14:16:50 -0500

    update role ui changes

--- src/components/UpdateRoleComponent/UpdateRoleComponent.tsx
+++ src/components/UpdateRoleComponent/UpdateRoleComponent.tsx
@@ -1,14 +1,21 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { updateRoleActionMapper } from '../../action-mappers/updateRole-action-mapper';
 import React, { FunctionComponent, useState, SyntheticEvent } from 'react'
-import { createStyles, makeStyles, Theme, InputLabel, Select, FormControl } from '@material-ui/core';
+import { createStyles, makeStyles, Theme, InputLabel, Select, FormControl, Container, createMuiTheme, ThemeProvider } from '@material-ui/core';
 import { IState } from '../../reducers';
+import { deepOrange } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         formControl: {
             margin: theme.spacing(1),
             minWidth: 120,
+        },
+        component: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '80vh'
         },
         selectEmpty: {
             marginTop: theme.spacing(2),
@@ -20,18 +27,23 @@ const useStyles = makeStyles((theme: Theme) =>
         input: {
             '&:before': {
                 borderRadius: 4,
-                borderColor: "#000000",
-                boxShadow: "#000000"
+                borderColor: "#f26926",
+                boxShadow: "#f26926"
             },
             '&:after': {
                 borderRadius: 4,
-                borderColor: "#000000",
-                boxShadow: "#000000",
-                name: "#000000"
+                borderColor: "#f26926",
+                boxShadow: "#f26926"
             }
         }
     }),
 );
+
+const theme = createMuiTheme({
+    palette: {
+        primary: deepOrange,
+    },
+});
 
 export const UpdateRoleComponent: FunctionComponent<any> = (props) => {
     const classes = useStyles();
@@ -60,21 +72,26 @@ export const UpdateRoleComponent: FunctionComponent<any> = (props) => {
         }
     }
     return (
-
-        <div>
-            <FormControl className={classes.formControl}>
-                <InputLabel htmlFor="age-native-simple">Role</InputLabel>
-                <Select className={classes.input} inputProps={{
-                    name: 'role',
-                    id: 'age-native-simple',
-                }}>
-                    <option value='Associate'>Associate</option>
-                    <option value='Trainer'>Trainer</option>
-                    <option value='Admin'>Admin</option>
-                </Select>
-            </FormControl>
-            <button className={classes.btn} color="default" type="submit" > Submit </button>
-        </div>
+        <Container className={classes.component} maxWidth="xs">
+            <div>
+                <h2>Update A Role</h2>
+                <FormControl className={classes.formControl}>
+                    <InputLabel htmlFor="age-native-simple">Role</InputLabel>
+                    <ThemeProvider theme={theme}>
+                        <Select className={classes.input} inputProps={{
+                            name: 'role',
+                            id: 'age-native-simple',
+                        }}>
+                            <option value='Associate'>Associate</option>
+                            <option value='Trainer'>Trainer</option>
+                            <option value='Admin'>Admin</option>
+                        </Select>
+                    </ThemeProvider>
+                </FormControl>
+                <br /><br />
+                <button className={classes.btn} color="default" type="submit" > Submit </button>
+            </div>
+        </Container>
     )
 
 }
